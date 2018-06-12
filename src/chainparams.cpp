a0029034c8e872990ffa493b5c834e83abcb39cf
ApollonMax <max.apollonxap@gmail.com>
2018-06-13 01:41:14 +0200

    beforesleep

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -186,7 +186,7 @@ public:
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = true;
+        fMiningRequiresPeers = false;
         fAllowMinDifficultyBlocks = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
