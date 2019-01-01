3a7c089282015b6ee2d6cd64ffe2f332d88f7a87
schnubbel2018 <44110482+schnubbel2018@users.noreply.github.com>
2019-01-01 20:30:02 +0100

    Update and rename zxapchain.h to zapmchain.h

--- src/zapmchain.h
+++ src/zapmchain.h
@@ -2,8 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef Apollon_ZXAPCHAIN_H
-#define Apollon_ZXAPCHAIN_H
+#ifndef Apayment_ZAPMCHAIN_H
+#define Apayment_ZAPMCHAIN_H
 
 #include "libzerocoin/Coin.h"
 #include "libzerocoin/Denominations.h"
@@ -39,4 +39,4 @@ bool TxOutToPublicCoin(const CTxOut& txout, libzerocoin::PublicCoin& pubCoin, CV
 std::list<libzerocoin::CoinDenomination> ZerocoinSpendListFromBlock(const CBlock& block, bool fFilterInvalid);
 
 
-#endif //Apollon_ZXAPCHAIN_H
+#endif //Apayment_ZAPMCHAIN_H
